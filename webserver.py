01318ce36dbe12f29b0cc1a41ea7208057eb76f1
Nathanaël Lécaudé <nat@studioimaginaire.com>
2013-06-21 15:25:50 -0400

    Add Json

--- webserver.py
+++ webserver.py
@@ -1,12 +1,14 @@
 from sht1x.Sht1x import Sht1x as SHT1x
 import web
+import json
 
 dataPin = 11
 clkPin = 7
 sht1x = SHT1x(dataPin, clkPin, SHT1x.GPIO_BOARD)
         
 urls = (
-    '/', 'atmo_server'
+    '/', 'atmo_server',
+    '/json', 'render_json'
 )
 app = web.application(urls, globals())
 
@@ -17,6 +19,13 @@ class atmo_server:
         temperature = sht1x.read_temperature_C()
         humidity = '{0:.2f}'.format(sht1x.read_humidity())
         return render.index(temperature, humidity)
+
+class render_json:        
+    def GET(self):
+        temperature = sht1x.read_temperature_C()
+        humidity = '{0:.2f}'.format(sht1x.read_humidity())
+        jsonData = json.dumps({'temperature': temperature, 'humidity': humidity})
+        return jsonData
 
 if __name__ == "__main__":
     app.run()
