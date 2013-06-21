63233c6251972133d74f39fcde80601ae01e171c
Nathanaël Lécaudé <nat@studioimaginaire.com>
2013-06-21 14:45:54 -0400

    Templates

--- webserver.py
+++ webserver.py
@@ -6,43 +6,15 @@ clkPin = 7
 sht1x = SHT1x(dataPin, clkPin, SHT1x.GPIO_BOARD)
         
 urls = (
-    '/(.*)', 'hello'
+    '/', 'atmo_server'
 )
 app = web.application(urls, globals())
 
-html = '''<html>
-  <head>
-    <script type='text/javascript' src='https://www.google.com/jsapi'></script>
-    <script type='text/javascript'>
-      google.load('visualization', '1', {{packages:['gauge']}});
-      google.setOnLoadCallback(drawChart);
-      function drawChart() {{
-        var data = google.visualization.arrayToDataTable([
-          ['Label', 'Value'],
-          ['Temp', {0}],
-          ['Humidity', {1:10.2f}]
-        ]);
+render = web.template.render('templates/')
 
-        var options = {{
-          width: 400, height: 120,
-          redFrom: 90, redTo: 100,
-          yellowFrom:75, yellowTo: 90,
-          minorTicks: 5
-        }};
-
-        var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }}
-    </script>
-  </head>
-  <body>
-    <div id='chart_div'></div>
-  </body>
-</html>'''
-
-class hello:        
-    def GET(self, name):
-        return html.format(sht1x.read_temperature_C(), sht1x.read_humidity())
+class atmo_server:        
+    def GET(self):
+        return render.index(sht1x.read_temperature_C(), sht1x.read_humidity())
 
 if __name__ == "__main__":
     app.run()
