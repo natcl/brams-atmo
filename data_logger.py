6f988f5ba1a7b11f4f8317ebef49d6dc81f49c95
Nathanaël Lécaudé <nat@studioimaginaire.com>
2013-06-26 19:59:12 -0400

    Print

--- data_logger.py
+++ data_logger.py
@@ -13,9 +13,10 @@ lcd.backlight_on()
 try:
     while(True):
         lcd.clear()
-        lcd.print('Temperature {0:.2f}'.format(sht1x.read_temperature_C()))
-        lcd.print('\n\r')
-        lcd.print('Humidity {0:.2f}'.format(sht1x.read_humidity()))
+        temperature = sht1x.read_temperature_C()
+        humidity = sht1x.read_humidity()
+        lcd.print(temperature)
+        lcd.print(humidity)
         time.sleep(2)
 except:
     print('Shutting down')
