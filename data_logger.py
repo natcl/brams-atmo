88e77c2b21470847e49983706104e7ec745b76ec
Nathanaël Lécaudé <nat@studioimaginaire.com>
2013-06-26 20:51:29 -0400

    settings

--- data_logger.py
+++ data_logger.py
@@ -9,18 +9,22 @@ sht1x = SHT1x(dataPin, clkPin, SHT1x.GPIO_BOARD)
 lcd = adaLCD('/dev/ttyACM0')
 lcd.clear()
 lcd.backlight_on()
+lcd.contrast(220)
+lcd.brightness(255)
+lcd.rgb(0,255,0)
 
 try:
     while(True):
         temperature = sht1x.read_temperature_C()
         humidity = sht1x.read_humidity()
         lcd.clear()
-        lcd.home()
         lcd.write('Temp {0:.2f}'.format(temperature))
         lcd.write('\n\r')
         lcd.write('Humidity {0:.2f}'.format(humidity))
         time.sleep(2)
+
 except KeyboardInterrupt:
     print('Shutting down')
     lcd.clear()
+    lcd.backlight_off()
     lcd.close()
