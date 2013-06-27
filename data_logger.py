77271613d7af673fb4b95089e3e274d79883050c
Nathanaël Lécaudé <nat@studioimaginaire.com>
2013-06-27 12:24:14 -0400

    logging

--- data_logger.py
+++ data_logger.py
@@ -1,10 +1,19 @@
 import time
+import logging
+from logging.handlers import TimedRotatingFileHandler
 from adaLCD import adaLCD
 from sht1x.Sht1x import Sht1x as SHT1x
 
 dataPin = 11
 clkPin = 7
 sht1x = SHT1x(dataPin, clkPin, SHT1x.GPIO_BOARD)
+
+logHandler = TimedRotatingFileHandler("atmo-piano.log",when="M", interval=2)
+logFormatter = logging.Formatter('%(asctime)s %(message)s')
+logHandler.setFormatter( logFormatter )
+logger = logging.getLogger( 'AtmoPiano' )
+logger.addHandler( logHandler )
+logger.setLevel( logging.INFO )
 
 lcd = adaLCD('/dev/ttyACM0')
 lcd.clear()
@@ -25,6 +34,7 @@ try:
         lcd.write('Temp:     {0:.2f}'.format(temperature))
         lcd.write('\n\r')
         lcd.write('Humidity: {0:.2f}'.format(humidity))
+        logger.info('T: {0:.2f} H: {1:.2f}'.format(temperature,humidity))
         time.sleep(2)
 
 except KeyboardInterrupt:
